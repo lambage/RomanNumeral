3f643c07a53d6ff9271e41222ddc53c909ab7165
Kris Lamb <kris.lamb@dejero.com>
2018-10-14 15:13:08 -0400

    working up to 4000

--- RomanNumeral/RomanNumeral.cpp
+++ RomanNumeral/RomanNumeral.cpp
@@ -10,7 +10,17 @@ std::string RomanNumeral::ConvertArabic(int number) const
 {
 	const auto table = std::vector<std::pair<int, std::string>>
 	{
+		std::make_pair(1000, "M"),
+		std::make_pair(900, "CM"),
+		std::make_pair(500, "D"),
+		std::make_pair(100, "C"),
+		std::make_pair(90, "XC"),
+		std::make_pair(50, "L"),
+		std::make_pair(40, "XL"),
 		std::make_pair(10, "X"),
+		std::make_pair(9, "IX"),
+		std::make_pair(5, "V"),
+		std::make_pair(4, "IV"),
 		std::make_pair(1, "I")
 	};
 	auto roman = std::string{};
--- RomanNumeralUnitTests/RomanNumeralUnitTests.cpp
+++ RomanNumeralUnitTests/RomanNumeralUnitTests.cpp
@@ -27,4 +27,27 @@ TEST_F(RomanNumeralUnitTest, CanConvert)
 	EXPECT_THAT(m_RomanNumeral->ConvertArabic(3), Eq("III"));
 	EXPECT_THAT(m_RomanNumeral->ConvertArabic(10), Eq("X"));
 	EXPECT_THAT(m_RomanNumeral->ConvertArabic(20), Eq("XX"));
+
+	EXPECT_THAT(m_RomanNumeral->ConvertArabic(5), Eq("V"));
+	EXPECT_THAT(m_RomanNumeral->ConvertArabic(9), Eq("IX"));
+	EXPECT_THAT(m_RomanNumeral->ConvertArabic(12), Eq("XII"));
+	EXPECT_THAT(m_RomanNumeral->ConvertArabic(16), Eq("XVI"));
+	EXPECT_THAT(m_RomanNumeral->ConvertArabic(29), Eq("XXIX"));
+	EXPECT_THAT(m_RomanNumeral->ConvertArabic(44), Eq("XLIV"));
+	EXPECT_THAT(m_RomanNumeral->ConvertArabic(45), Eq("XLV"));
+	EXPECT_THAT(m_RomanNumeral->ConvertArabic(68), Eq("LXVIII"));
+	EXPECT_THAT(m_RomanNumeral->ConvertArabic(83), Eq("LXXXIII"));
+	EXPECT_THAT(m_RomanNumeral->ConvertArabic(97), Eq("XCVII"));
+	EXPECT_THAT(m_RomanNumeral->ConvertArabic(99), Eq("XCIX"));
+	EXPECT_THAT(m_RomanNumeral->ConvertArabic(500), Eq("D"));
+	EXPECT_THAT(m_RomanNumeral->ConvertArabic(501), Eq("DI"));
+	EXPECT_THAT(m_RomanNumeral->ConvertArabic(649), Eq("DCXLIX"));
+	EXPECT_THAT(m_RomanNumeral->ConvertArabic(798), Eq("DCCXCVIII"));
+	EXPECT_THAT(m_RomanNumeral->ConvertArabic(891), Eq("DCCCXCI"));
+	EXPECT_THAT(m_RomanNumeral->ConvertArabic(1000), Eq("M"));
+	EXPECT_THAT(m_RomanNumeral->ConvertArabic(1004), Eq("MIV"));
+	EXPECT_THAT(m_RomanNumeral->ConvertArabic(1006), Eq("MVI"));
+	EXPECT_THAT(m_RomanNumeral->ConvertArabic(1023), Eq("MXXIII"));
+	EXPECT_THAT(m_RomanNumeral->ConvertArabic(2014), Eq("MMXIV"));
+	EXPECT_THAT(m_RomanNumeral->ConvertArabic(3999), Eq("MMMCMXCIX"));
 }
