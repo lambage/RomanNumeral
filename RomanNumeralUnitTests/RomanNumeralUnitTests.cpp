fc026c177dd01e732c7a9136eb0d50d27426168d
Kris Lamb <kris.lamb@dejero.com>
2018-10-14 14:36:11 -0400

    I was close to having a perfect first commit...

--- RomanNumeralUnitTests/RomanNumeralUnitTests.cpp
+++ RomanNumeralUnitTests/RomanNumeralUnitTests.cpp
@@ -1,11 +1,27 @@
 #include "gmock/gmock.h"
+#include "RomanNumeral/RomanNumeral.hpp"
 
 using namespace ::testing;
+using namespace dejero::tools;
 
 class RomanNumeralUnitTest : public ::testing::Test
 {
+protected:
+	void SetUp() override
+	{
+		m_RomanNumeral = std::make_unique<RomanNumeral>();
+	}
+
+	void TearDown() override
+	{
+		m_RomanNumeral = nullptr;
+	}
+
+	std::unique_ptr<IRomanNumeral> m_RomanNumeral;
 };
 
 TEST_F(RomanNumeralUnitTest, CanConvert)
 {
+	// EXPECT_THAT will continue running without returning, so we will use that since
+	// there aren't many differences in the tests
 }
