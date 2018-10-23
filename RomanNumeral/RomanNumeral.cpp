5187aa2d428c0b44369422860ded3f35abcebd18
Kris Lamb <kris.lamb@dejero.com>
2018-10-23 14:04:01 -0400

    the solution we came up with together as a group

--- RomanNumeral/RomanNumeral.cpp
+++ RomanNumeral/RomanNumeral.cpp
@@ -1,4 +1,5 @@
 #include "RomanNumeral/RomanNumeral.hpp"
+#include <vector>
 
 namespace dejero
 {
@@ -7,7 +8,38 @@ namespace tools
 	
 std::string RomanNumeral::ConvertArabic(int number) const
 {
-	return "I";
+	if (number < 1 || number >= 4000)
+	{
+		throw std::runtime_error("number out of range");
+	}
+	std::string roman = {};
+
+	const std::vector<std::pair<std::string, int>> conversionMap =
+	{
+		std::make_pair<std::string, int>("M", 1000),
+		std::make_pair<std::string, int>("CM", 900),
+		std::make_pair<std::string, int>("D", 500),
+		std::make_pair<std::string, int>("CD", 400),
+		std::make_pair<std::string, int>("C", 100),
+		std::make_pair<std::string, int>("XC", 90),
+		std::make_pair<std::string, int>("L", 50),
+		std::make_pair<std::string, int>("XL", 40),
+		std::make_pair<std::string, int>("X", 10),
+		std::make_pair<std::string, int>("IX", 9),
+		std::make_pair<std::string, int>("V", 5),
+		std::make_pair<std::string, int>("IV", 4),
+		std::make_pair<std::string, int>("I", 1)
+	};
+
+	for (auto it : conversionMap)
+	{
+		while (number >= it.second)
+		{
+			roman += it.first;
+			number -= it.second;
+		}
+	}
+	return roman;
 }
 
 } // namespace tools
--- RomanNumeralUnitTests/RomanNumeralUnitTests.cpp
+++ RomanNumeralUnitTests/RomanNumeralUnitTests.cpp
@@ -24,4 +24,47 @@ TEST_F(RomanNumeralUnitTest, CanConvert)
 {
 	// EXPECT_THAT will continue running without returning, so we will use that since
 	// there aren't many differences in the tests
+	EXPECT_THAT(m_RomanNumeral->ConvertArabic(4), Eq("IV"));
+	EXPECT_THAT(m_RomanNumeral->ConvertArabic(2), Eq("II"));
+	EXPECT_THAT(m_RomanNumeral->ConvertArabic(3), Eq("III"));
+	EXPECT_THAT(m_RomanNumeral->ConvertArabic(1), Eq("I"));
+	EXPECT_THAT(m_RomanNumeral->ConvertArabic(5), Eq("V"));
+	EXPECT_THAT(m_RomanNumeral->ConvertArabic(6), Eq("VI"));
+	EXPECT_THAT(m_RomanNumeral->ConvertArabic(9), Eq("IX"));
+	EXPECT_THAT(m_RomanNumeral->ConvertArabic(10), Eq("X"));
+	EXPECT_THAT(m_RomanNumeral->ConvertArabic(100), Eq("C"));
+
+	EXPECT_THAT(m_RomanNumeral->ConvertArabic(5), Eq("V"));
+	EXPECT_THAT(m_RomanNumeral->ConvertArabic(9), Eq("IX"));
+	EXPECT_THAT(m_RomanNumeral->ConvertArabic(12), Eq("XII"));
+	EXPECT_THAT(m_RomanNumeral->ConvertArabic(16), Eq("XVI"));
+	EXPECT_THAT(m_RomanNumeral->ConvertArabic(29), Eq("XXIX"));
+	EXPECT_THAT(m_RomanNumeral->ConvertArabic(44), Eq("XLIV"));
+	EXPECT_THAT(m_RomanNumeral->ConvertArabic(45), Eq("XLV"));
+	EXPECT_THAT(m_RomanNumeral->ConvertArabic(68), Eq("LXVIII"));
+	EXPECT_THAT(m_RomanNumeral->ConvertArabic(83), Eq("LXXXIII"));
+	EXPECT_THAT(m_RomanNumeral->ConvertArabic(97), Eq("XCVII"));
+	EXPECT_THAT(m_RomanNumeral->ConvertArabic(99), Eq("XCIX"));
+	EXPECT_THAT(m_RomanNumeral->ConvertArabic(500), Eq("D"));
+	EXPECT_THAT(m_RomanNumeral->ConvertArabic(501), Eq("DI"));
+	EXPECT_THAT(m_RomanNumeral->ConvertArabic(649), Eq("DCXLIX"));
+	EXPECT_THAT(m_RomanNumeral->ConvertArabic(798), Eq("DCCXCVIII"));
+	EXPECT_THAT(m_RomanNumeral->ConvertArabic(891), Eq("DCCCXCI"));
+	EXPECT_THAT(m_RomanNumeral->ConvertArabic(1000), Eq("M"));
+	EXPECT_THAT(m_RomanNumeral->ConvertArabic(1004), Eq("MIV"));
+	EXPECT_THAT(m_RomanNumeral->ConvertArabic(1006), Eq("MVI"));
+	EXPECT_THAT(m_RomanNumeral->ConvertArabic(1023), Eq("MXXIII"));
+	EXPECT_THAT(m_RomanNumeral->ConvertArabic(2014), Eq("MMXIV"));
+	EXPECT_THAT(m_RomanNumeral->ConvertArabic(3999), Eq("MMMCMXCIX"));
 }
+
+TEST_F(RomanNumeralUnitTest, DoesNotConvert)
+{
+	// EXPECT_THAT will continue running without returning, so we will use that since
+	// there aren't many differences in the tests
+	EXPECT_THROW(m_RomanNumeral->ConvertArabic(0), std::runtime_error);
+	EXPECT_THROW(m_RomanNumeral->ConvertArabic(-1), std::runtime_error);
+	EXPECT_THROW(m_RomanNumeral->ConvertArabic(-100), std::runtime_error);
+	EXPECT_THROW(m_RomanNumeral->ConvertArabic(4000), std::runtime_error);
+}
+
